22af037536e2829ad0d11a77cecccef8dd9e63cf
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2024-01-15 03:10:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
 $('.juri-slider').slick({
+  dots: false,
   infinite: true,
+  speed: 500,
   slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 300,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, 
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 $("#nomitant").click(function() { // ID откуда кливаем
